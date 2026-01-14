2d85033da9efdb74e9ea69fe27307b272d26383b
mishra-anant <mishra.anant.hbtu2k23@gmail.com>
2026-01-14 14:55:59 +0530

    Initial commit

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,17 @@
+import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
+import Map from "../components/Map";
+
+function AppLayout() {
+  return (
+    <>
+      <div className={styles.app}>
+        <Sidebar></Sidebar>
+        <Map></Map>
+      </div>
+    </>
+  );
+}
+
+export default AppLayout;
